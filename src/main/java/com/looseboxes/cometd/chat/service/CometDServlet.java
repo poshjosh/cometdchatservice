bf4656790ea0a6b63aac499eb9687f2c6612c53b
poshjosh <posh.bc@gmail.com>
2020-02-04 16:55:23 +0100

    Remove Bayeux server initialization logic. Logic now handled by a listener

--- src/main/java/com/looseboxes/cometd/chat/service/CometDServlet.java
+++ src/main/java/com/looseboxes/cometd/chat/service/CometDServlet.java
@@ -26,24 +26,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.UnavailableException;
 import javax.servlet.http.HttpServlet;
 
-import org.cometd.annotation.Configure;
-import org.cometd.annotation.Listener;
-import org.cometd.annotation.RemoteCall;
-import org.cometd.annotation.ServerAnnotationProcessor;
-import org.cometd.annotation.Service;
-import org.cometd.bayeux.Message;
-import org.cometd.bayeux.server.BayeuxServer;
-import org.cometd.bayeux.server.ConfigurableServerChannel;
-import org.cometd.bayeux.server.ServerChannel;
-import org.cometd.bayeux.server.ServerMessage;
-import org.cometd.bayeux.server.ServerSession;
-import org.cometd.server.BayeuxServerImpl;
-import org.cometd.server.authorizer.GrantAuthorizer;
-import org.cometd.server.ext.AcknowledgedMessagesExtension;
-import org.cometd.server.ext.TimesyncExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -53,80 +37,6 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 public final class CometDServlet extends HttpServlet {
     
     private static final Logger LOG = LoggerFactory.getLogger(CometDServlet.class);
-    
-    @Override
-    public void init() throws ServletException {
-        
-        super.init();
-        
-        final ServletContext servletContext = getServletContext();
-        
-        final BayeuxServerImpl bayeux = (BayeuxServerImpl)servletContext.getAttribute(BayeuxServer.ATTRIBUTE);
-
-        if (bayeux == null) {
-            throw new UnavailableException("No BayeuxServer!");
-        }
-
-        // Create extensions
-        bayeux.addExtension(new TimesyncExtension());
-        bayeux.addExtension(new AcknowledgedMessagesExtension());
-
-        // Deny unless granted
-        bayeux.createChannelIfAbsent("/**", (ServerChannel.Initializer)channel -> channel.addAuthorizer(GrantAuthorizer.GRANT_NONE));
-//        bayeux.createChannelIfAbsent("/**", (ServerChannel.Initializer)channel -> channel.addAuthorizer(GrantAuthorizer.GRANT_ALL));
-
-        // Allow anybody to handshake
-        bayeux.getChannel(ServerChannel.META_HANDSHAKE).addAuthorizer(GrantAuthorizer.GRANT_PUBLISH);
-//        bayeux.getChannel(ServerChannel.META_HANDSHAKE).addAuthorizer(GrantAuthorizer.GRANT_ALL);
-
-        ServerAnnotationProcessor processor = new ServerAnnotationProcessor(bayeux);
-        processor.process(new EchoRPC());
-        processor.process(new Monitor());
-
-        final CometDProperties cometdProperties = this.getBean(servletContext, CometDProperties.class);
-        
-        bayeux.createChannelIfAbsent(cometdProperties.getDefaultChannel(), new ConfigurableServerChannel.Initializer.Persistent());
-//        bayeux.createChannelIfAbsent(cometdProperties.getDefaultChannel(), (ServerChannel.Initializer)channel -> channel.addAuthorizer(GrantAuthorizer.GRANT_ALL));
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug(bayeux.dump());
-        }
-    }
-
-    @Service("echo")
-    public static class EchoRPC {
-        
-        @Configure("/service/echo")
-        private void configureEcho(ConfigurableServerChannel channel) {
-            channel.addAuthorizer(GrantAuthorizer.GRANT_SUBSCRIBE_PUBLISH);
-        }
-
-        @RemoteCall("echo")
-        public void doEcho(RemoteCall.Caller caller, Object data) {
-            LOG.info("ECHO from " + caller.getServerSession() + ": " + data);
-            caller.result(data);
-        }
-    }
-
-    @Service("monitor")
-    public static class Monitor {
-        @Listener("/meta/subscribe")
-        public void monitorSubscribe(ServerSession session, ServerMessage message) {
-            LOG.info("Monitored Subscribe from " + session + " for " + message.get(Message.SUBSCRIPTION_FIELD));
-        }
-
-        @Listener("/meta/unsubscribe")
-        public void monitorUnsubscribe(ServerSession session, ServerMessage message) {
-            LOG.info("Monitored Unsubscribe from " + session + " for " + message.get(Message.SUBSCRIPTION_FIELD));
-        }
-
-        @Listener("/meta/*")
-        public void monitorMeta(ServerSession session, ServerMessage message) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(message.toString());
-            }
-        }
-    }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
