3efaa8c5a15bf9fdb3097cd76ce9490709040d8f
poshjosh <posh.bc@gmail.com>
2020-02-05 02:46:12 +0100

    Catch exception and return appropriate json response

--- src/main/java/com/looseboxes/cometd/chat/service/handlers/request/AbstractRequestHandler.java
+++ src/main/java/com/looseboxes/cometd/chat/service/handlers/request/AbstractRequestHandler.java
@@ -15,15 +15,20 @@
  */
 package com.looseboxes.cometd.chat.service.handlers.request;
 
+import com.looseboxes.cometd.chat.service.AttributeNames;
 import com.looseboxes.cometd.chat.service.handlers.response.Response;
 import com.looseboxes.cometd.chat.service.handlers.response.ResponseHandler;
 import com.looseboxes.cometd.chat.service.handlers.exceptions.ProcessingRequestException;
+import com.looseboxes.cometd.chat.service.handlers.response.ResponseBuilder;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.cometd.bayeux.client.ClientSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * @author USER
@@ -35,7 +40,45 @@ public abstract class AbstractRequestHandler implements RequestHandler<Response>
     public AbstractRequestHandler() { }
 
     public abstract Response doProcess(HttpServletRequest req, HttpServletResponse res) throws ProcessingRequestException;
+    
+    /**
+     * @param req
+     * @param res
+     * @return {@link com.looseboxes.cometd.chat.service.handlers.response.Response Response} 
+     * object with success set to true if previously joined to chat or
+     * successfully joined to chat during this methods execution, otherwise 
+     * return false.
+     */
+    public Response joinIfNotAlready(HttpServletRequest req, HttpServletResponse res) {
+        
+        final WebApplicationContext webAppCtx = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(req.getServletContext());
+        
+        final boolean joinedToChat = this.isJoinedToChat(req, res);
+        
+        if( ! joinedToChat) {
+        
+            final JoinHandler jh = webAppCtx.getBean(JoinHandler.class);
 
+            final Response jhr = jh.process(req, res);
+
+            if(!jhr.isSuccess()) {
+
+                return jhr;
+            }
+        }
+        
+        return webAppCtx.getBean(ResponseBuilder.class).buildSuccessResponse();
+    }
+
+    public boolean isJoinedToChat(HttpServletRequest req, HttpServletResponse res) {
+        
+        final ClientSession client = (ClientSession)req.getSession().getAttribute(
+                AttributeNames.Session.COMETD_CLIENT_SESSION);
+        
+        return client == null ? false : client.isHandshook();
+    }
+    
     @Override
     public void process(ServletRequest req, ServletResponse res, ResponseHandler<Response> callback) {
         
@@ -51,18 +94,25 @@ public abstract class AbstractRequestHandler implements RequestHandler<Response>
     }
 
     @Override
-    public Response process(ServletRequest req, ServletResponse res) throws ProcessingRequestException{
-    
-        if(req instanceof HttpServletRequest) {
-        
-            final HttpServletRequest httpReq = (HttpServletRequest)req;
-            
-            return this.doProcess(httpReq, (HttpServletResponse)res);
-            
-        }else{
+    public Response process(ServletRequest req, ServletResponse res) {
+        try{
+            if(req instanceof HttpServletRequest) {
 
-            //@TODO
-            throw new UnsupportedOperationException();
+                final HttpServletRequest httpReq = (HttpServletRequest)req;
+
+                return this.doProcess(httpReq, (HttpServletResponse)res);
+
+            }else{
+
+                //@TODO
+                throw new UnsupportedOperationException();
+            }
+        }catch(ProcessingRequestException | UnsupportedOperationException e) {
+        
+            final WebApplicationContext webAppCtx = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(req.getServletContext());
+
+            return webAppCtx.getBean(ResponseBuilder.class).buildErrorResponse(e);
         }
     }
 }
