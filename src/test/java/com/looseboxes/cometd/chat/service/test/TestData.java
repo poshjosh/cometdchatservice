a028304f439aae1c4a2a34fa6477e5be3633b0da
poshjosh <posh.bc@gmail.com>
2020-02-11 00:11:09 +0100

    Make minor refactorings

--- src/test/java/com/looseboxes/cometd/chat/service/test/TestData.java
+++ src/test/java/com/looseboxes/cometd/chat/service/test/TestData.java
@@ -19,7 +19,7 @@ import com.looseboxes.cometd.chat.service.handlers.response.Response;
 import com.looseboxes.cometd.chat.service.handlers.response.ResponseImpl;
 import javax.servlet.http.HttpServletResponse;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author USER
