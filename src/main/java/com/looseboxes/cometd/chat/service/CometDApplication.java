c7fc808e154c5e1706a911eeb586ae78543f75df
poshjosh <posh.bc@gmail.com>
2020-02-11 03:38:32 +0100

    Add annotation @EnableCaching

--- src/main/java/com/looseboxes/cometd/chat/service/CometDApplication.java
+++ src/main/java/com/looseboxes/cometd/chat/service/CometDApplication.java
@@ -23,9 +23,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+@EnableCaching
 @ServletComponentScan
 @SpringBootApplication
 @EnableDiscoveryClient
