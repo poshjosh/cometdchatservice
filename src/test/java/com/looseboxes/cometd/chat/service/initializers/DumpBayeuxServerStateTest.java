a275a8640c5512f4b8b682f9e80cd101f65473fc
poshjosh <posh.bc@gmail.com>
2020-02-25 12:39:51 +0100

    Add test for Bayeux initialization logic

---
+++ src/test/java/com/looseboxes/cometd/chat/service/initializers/DumpBayeuxServerStateTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 looseBoxes.com
+ *
+ * Licensed under the looseBoxes Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.looseboxes.cometd.chat.service.initializers;
+
+import java.util.Collections;
+import java.util.List;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.server.BayeuxServerImpl;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author USER
+ */
+public class DumpBayeuxServerStateTest extends BayeuxInitActionMockTestBase{
+    
+    private static final String SAMPLE_DUMP = "Sample Dump";
+       
+    private static final class ContextImpl implements BayeuxInitActionMockTestBase.Context{
+        private final BayeuxInitActionMockTestBase test;
+        public ContextImpl() {
+            this(null);
+        }
+        public ContextImpl(BayeuxInitActionMockTestBase test) {
+            this.test = test;
+        }
+        @Override
+        public BayeuxInitActionMockTestBase.Context with(BayeuxInitActionMockTestBase test) {
+            return new ContextImpl(test);
+        }
+        @Override
+        public List getArgs(){
+            return Collections.EMPTY_LIST;
+        }
+        @Override
+        public void onApplyMethodCalled(BayeuxServer server, List args) {
+            if(server instanceof BayeuxServerImpl) {
+                ((BayeuxServerImpl)server).dump();
+            }
+        }
+        @Override
+        public void assertThatResultsAreValid(BayeuxServer server, List args) { }
+    }
+    
+    public DumpBayeuxServerStateTest() { 
+        super(new ContextImpl());
+    }
+
+    @Override
+    public DumpBayeuxServerState mockBayeuxInitAction() {
+        final DumpBayeuxServerState bayeuxInitAction = mock(DumpBayeuxServerState.class);
+        return bayeuxInitAction;
+    }
+    
+    @Override
+    public BayeuxServer mockBayeuxServer(List args) {
+        final BayeuxServerImpl bayeuxServer = mock(BayeuxServerImpl.class);
+        //@TODO remove lenient... Without lenient throws UnnecessaryStubbingException
+        lenient().when(bayeuxServer.dump()).thenReturn(SAMPLE_DUMP);
+        return bayeuxServer;
+    } 
+}
+
---
+++ src/test/java/com/looseboxes/cometd/chat/service/initializers/ProcessAnnotatedServicesTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2020 looseBoxes.com
+ *
+ * Licensed under the looseBoxes Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.looseboxes.cometd.chat.service.initializers;
+
+import com.looseboxes.cometd.chat.service.MembersServiceInMemoryCache;
+import java.util.Arrays;
+import java.util.List;
+import org.cometd.annotation.Configure;
+import org.cometd.annotation.RemoteCall;
+import org.cometd.annotation.ServerAnnotationProcessor;
+import org.cometd.annotation.Service;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ConfigurableServerChannel;
+import static org.mockito.Mockito.mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author USER
+ */
+public class ProcessAnnotatedServicesTest extends BayeuxInitActionMockTestBase{
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ProcessAnnotatedServicesTest.class);
+    
+    private static final class ContextImpl implements BayeuxInitActionMockTestBase.Context{
+        private final BayeuxInitActionMockTestBase test;
+        public ContextImpl() {
+            this(null);
+        }
+        public ContextImpl(BayeuxInitActionMockTestBase test) {
+            this.test = test;
+        }
+        @Override
+        public BayeuxInitActionMockTestBase.Context with(BayeuxInitActionMockTestBase test) {
+            return new ContextImpl(test);
+        }
+        @Override
+        public List getArgs(){
+            return Arrays.asList(new MembersServiceInMemoryCache());
+        }
+        @Override
+        public void onApplyMethodCalled(BayeuxServer server, List args) {
+            final ServerAnnotationProcessor processor = new ServerAnnotationProcessor(server);
+            processor.process(new DummyAnnotatedService());
+        }
+        @Override
+        public void assertThatResultsAreValid(BayeuxServer server, List args) { }
+    }
+    
+    public ProcessAnnotatedServicesTest() { 
+        super(new ContextImpl());
+    }
+
+    @Override
+    public ProcessAnnotatedServices mockBayeuxInitAction() {
+        final ProcessAnnotatedServices bayeuxInitAction = mock(ProcessAnnotatedServices.class);
+        return bayeuxInitAction;
+    }
+    
+    @Service("DummyAnnotatedService")
+    private static final class DummyAnnotatedService{
+        @Configure("/service/echo")
+        private void configureEcho(ConfigurableServerChannel channel) {
+            LOG.info("Configuring: {}", channel);
+        }
+        @RemoteCall("echo")
+        public void doEcho(RemoteCall.Caller caller, Object data) {
+            LOG.info("ECHO from " + caller.getServerSession() + ": " + data);
+            caller.result(data);
+        }
+    }
+}
+
+
