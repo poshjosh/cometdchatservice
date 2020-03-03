14d5212b98e35e828c2e6bc345abaec24b403826
poshjosh <posh.bc@gmail.com>
2020-03-03 20:10:41 +0100

    Add controller integration tests

---
+++ src/test/java/com/looseboxes/cometd/chatservice/controllers/ChatControllerIT.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 looseBoxes.com
+ *
+ * Licensed under the looseBoxes Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.looseboxes.cometd.chatservice.controllers;
+
+import com.looseboxes.cometd.chatservice.handlers.request.RequestHandler;
+import com.looseboxes.cometd.chatservice.handlers.request.RequestHandlerQualifiers;
+import com.looseboxes.cometd.chatservice.handlers.response.Response;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+/**
+ * @author USER
+ */
+@WebMvcTest(controllers = ChatController.class)
+public class ChatControllerIT extends AbstractControllerIT{
+    
+    /** Required by the Controller being tested */
+    @Qualifier(RequestHandlerQualifiers.CHAT_HANDLER)
+    @MockBean private RequestHandler<Response> requestHandler;
+    
+    @Test
+    public void requestToChatEndpoint_whenParamsValid_shouldReturnSuccessfully() {
+        this.requestToEndpoint_whenParamsValid_shouldReturnSuccessfully(Endpoints.CHAT);
+    }
+
+    @Test
+    public void requestToChatEndpoint_whenParamsNotValid_shouldReturnErrorResponse() {
+        this.requestToEndpoint_whenParamsGiven_shouldReturnMatchingResult(
+                Endpoints.CHAT, 500, Collections.EMPTY_MAP);
+    }
+
+    @Override
+    public RequestHandler<Response> getRequestHandler() {
+        return requestHandler;
+    }
+}
