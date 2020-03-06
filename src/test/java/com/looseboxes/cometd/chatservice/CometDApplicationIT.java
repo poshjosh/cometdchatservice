bab3d6673f5a6282bf831da9b2348986918b973a
poshjosh <posh.bc@gmail.com>
2020-03-06 18:10:16 +0100

    Fix Unable to find @SpringBootConfiguration

--- src/test/java/com/looseboxes/cometd/chatservice/CometDApplicationIT.java
+++ src/test/java/com/looseboxes/cometd/chatservice/CometDApplicationIT.java
@@ -44,7 +44,7 @@ import static org.mockito.ArgumentMatchers.*;
 /**
  * @author USER
  */
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes=CometDApplication.class)
 @Import(MyTestConfiguration.class)
 public class CometDApplicationIT {
     
