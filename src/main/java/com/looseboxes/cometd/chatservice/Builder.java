e51fd68590ba0221249b1aeacec9c02c3c6ac2e4
poshjosh <posh.bc@gmail.com>
2020-03-11 23:44:18 +0100

    Add comments to builder

--- src/main/java/com/looseboxes/cometd/chatservice/Builder.java
+++ src/main/java/com/looseboxes/cometd/chatservice/Builder.java
@@ -16,11 +16,25 @@
 package com.looseboxes.cometd.chatservice;
 
 /**
+ * The parent specification for Builder types.
  * @author USER
+ * @param <T> The type of the instances this Builder builds and returns via the
+ * {@link #build()} method.
  */
 public interface Builder<T> {
 
+    /**
+     * This method may only be called once per Builder, otherwise it will throw
+     * an exception. So if in doubt use {@link #isBuildAttempted()} to confirm 
+     * if this method has already been called.
+     * @return The newly created instance.
+     * @see #isBuildAttempted() 
+     */
     T build();
     
+    /**
+     * @return <code>true</code> if the {@link #build()} method has already been
+     * called, otherwise returns <code>false</code>
+     */
     boolean isBuildAttempted();
 }
