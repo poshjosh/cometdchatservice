7a2edda7dd8912d59fff7788b59fd8e1a46644c2
poshjosh <posh.bc@gmail.com>
2020-03-13 17:01:29 +0100

    Make SmokeTest integration test. Rename to SmokeIT

--- src/test/java/com/looseboxes/cometd/chatservice/SmokeIT.java
+++ src/test/java/com/looseboxes/cometd/chatservice/SmokeIT.java
@@ -22,12 +22,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author USER
  */
 @SpringBootTest
-public class SmokeTest {
+public class SmokeIT {
 
     @Autowired private JoinController joinController;
     @Autowired private ChatController chatController;
