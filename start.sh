bef0b982fd71be145492f2c243ca20350d8a1d3e
poshjosh <posh.bc@gmail.com>
2020-04-02 21:10:54 +0100

    Return to workspace root after multiple calls to cd

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ if [ "$DEBUG" = true ] ; then
     echo 'ls -a' && ls -a 
     echo 'cd lib' && cd lib 
     echo 'ls -a' && ls -a
+    echo 'cd .. && cd ..'
+    cd .. && cd ..
 fi
 echo java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS} ${@}
 exec java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS} ${@}
