1ad35422e45dace238d9f7fe37f36bbfbc01a7cb
poshjosh <posh.bc@gmail.com>
2020-04-02 18:01:14 +0100

    Add forward slash to each classpath element

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS} ${@}
-exec java ${JAVA_OPTS} -cp app:app/lib/ ${MAIN_CLASS}
+ls -a && cd app && ls -a && cd lib && ls -a
+exec java ${JAVA_OPTS} -cp /app:/app/lib/ ${MAIN_CLASS}
