c076528290b6d19810a9865fafa0da85d52d3ad1
poshjosh <posh.bc@gmail.com>
2020-04-02 17:20:59 +0100

    Remove command line args

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo 'java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS} ${@}'
-exec java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS} ${@}
+exec java ${JAVA_OPTS} -cp app:app/lib/* ${MAIN_CLASS}
